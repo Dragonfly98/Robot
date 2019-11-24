f096fd0ece58d9a5096455f6c2d896f64485f1b2
Dragonfly98 <57970756+Dragonfly98@users.noreply.github.com>
2019-11-24 18:38:11 -0500

    Update makeblocktest.ino

--- makeblocktest.ino
+++ makeblocktest.ino
@@ -1,13 +1,15 @@
 #include "MeOrion.h"
 #include <Wire.h>
 #include <SoftwareSerial.h>
-/*
+
 MeDCMotor motor1(M1);
 MeDCMotor motor2(M2);
-uint8_t motorSpeed=100;
-*/
-MeUltrasonicSensor ultraSensor(PORT_8);
+uint8_t motorSpeed=150;
 
+//MeUltrasonicSensor sensors;
+MeUltrasonicSensor ultraSensor2(PORT_7);
+//int distance;
+int distance2;
 
 void setup()
 {
@@ -16,22 +18,42 @@ void setup()
 
 void loop()
 {
+  /*
   Serial.print("Distance: ");
-  Serial.print(ultraSensor.distanceCm() );
+  Serial.print(ultraSensor2.distanceCm() );
   Serial.println(" cm");
   delay(100);
-  /*
-  motor1.run(-motorSpeed);
-  motor2.run(motorSpeed);
-  delay(2000);
-  motor1.stop();
-  motor2.stop();
-  delay(2000);
-  motor1.run(motorSpeed);
-  motor2.run(-motorSpeed);
-  delay(2000);
-  motor1.stop();
-  motor2.stop();
-  delay(2000);
   */
+  
+  //distance = ultraSensor.distanceCm();
+  distance2 = ultraSensor2.distanceCm();
+  /*
+  if (distance < 25) 
+  {
+    motor1.run(-motorSpeed);
+    motor2.run(motorSpeed);
+    delay(500);
+    motor1.run(motorSpeed*2);
+    motor2.run(motorSpeed*2);
+    delay(1500);
+  }
+  else
+  */
+  if (distance2 < 25)
+  {
+    motor1.run(-motorSpeed);
+    motor2.run(motorSpeed);
+    delay(500);
+    motor1.run(motorSpeed*2);
+    motor2.run(motorSpeed*2);
+    delay(1500);
+  }
+  else 
+  {
+    motor1.run(motorSpeed);
+    motor2.run(-motorSpeed);
+    delay(100);
+  }
+
+ 
 }
